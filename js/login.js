28823bcc8c4d18169c5c18975999b2c20f35906f
islomiddindeveloper <islomiddincoder@gmail.com>
2023-04-04 11:26:12 +0500

    first commit

--- js/login.js
+++ js/login.js
@@ -51,11 +51,3 @@ elForm.addEventListener("submit", (evt) => {
 
 
 
-
-
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/islomiddindeveloper/islomiddin-imtihon-4.git
-// git push -u origin main
